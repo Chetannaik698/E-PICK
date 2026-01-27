e5cdaf04b130491fe85d94171a241254fc176ea9
Chetan Naik <chetannaik11145@gmail.com>
2026-01-27 21:10:04 +0530

    improved styling in home page

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
     
 const Collection = () => {
   return (
     <div>
+      <Navbar />
       <h1>Collection Page</h1>
     </div>
   )
