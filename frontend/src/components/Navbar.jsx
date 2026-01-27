a3e4d5456d1b6104bace5064536e1baa29029c9c
Chetan Naik <chetannaik11145@gmail.com>
2026-01-27 13:52:17 +0530

    home-branch

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div style={{ backgroundColor: "#333", color: "#fff", padding: "10px" }}>
+      <h1>Navbar</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 const Home = () => {
   return (
     <div>
+      <Navbar />
       <h1>Welcome to the Home Page</h1>
     </div>
   )
