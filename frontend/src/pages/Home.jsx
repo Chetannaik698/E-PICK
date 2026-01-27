d852404de38ae94352aae745bc78078d485b8b74
Chetan Naik <chetannaik11145@gmail.com>
2026-01-27 14:26:24 +0530

    added home content

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
 import '../styles/Home.css'
 
 const Home = () => {
+
   return (
     <div>
       <Navbar />
       
+      <section className='hero'>
+        <div className="left">left</div>
+        <div className="right">right</div>
+      </section>
+
     </div>
   )
 }
