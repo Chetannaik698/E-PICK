c411db34c46a1301b0e9026c4950788de07c76a1
Chetan Naik <chetannaik11145@gmail.com>
2026-01-27 14:18:55 +0530

    navbar created

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,36 @@
 import React from "react";
+import logo from "../assets/logo.png";
+import search from "../assets/search.png";
+import profile from "../assets/profile.png";
+import cart from "../assets/cart.png";
+import "../styles/Navbar.css";
 
 const Navbar = () => {
   return (
-    <div style={{ backgroundColor: "#333", color: "#fff", padding: "10px" }}>
-      <h1>Navbar</h1>
-    </div>
+    <header>
+      <nav>
+        <div className="logo">
+          <img src={logo} alt="Logo" />
+        </div>
+
+        <div className="nav-items">
+          <a href="">HOME</a>
+          <a href="">COLLECTION</a>
+          <a href="">ABOUT</a>
+          <a href="">CONTACT</a>
+
+          <div className="admin">
+            <span>Admin Panel</span>
+          </div>
+        </div>
+
+        <div className="nav-functions">
+          <img src={search} alt="" />
+          <img src={profile} alt="" />
+          <img src={cart} alt="" />
+        </div>
+      </nav>
+    </header>
   );
 };
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import '../styles/Home.css'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <h1>Welcome to the Home Page</h1>
+      
     </div>
   )
 }
