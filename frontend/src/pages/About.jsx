b7c1e7339d6de0b23aa3e19dd06e42328e3a83c5
PalakshShetty1 <shettypalakash5@email.com>
2026-01-28 15:47:48 +0530

    initial about pagess

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,17 +1,51 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import "../styles/About.css";
+import image1 from "../assets/about.png";
 
 const About = () => {
   return (
     <div>
       <Navbar />
-      <h1>About Us</h1>
-      <p></p>
       <div className="main">
-        h1 sdd ccc ccc
-        <div className="left"></div>
-        <div className="right"></div>
+        <div className="up">
+          <h2>
+            {" "}
+            ABOUT <b>US</b>
+          </h2>
+        </div>
+        <div className="down">
+          <div className="down-1">
+            <img src={image1} alt="" />
+          </div>
+          <div className="down-2">
+            <p>
+              Forever was born out of a passion for innovation and a desire to
+              revolutionize the way people shop online. Our journey began with a
+              simple idea: to provide a platform where customers can easily
+              discover, explore, and purchase a wide range of products from the
+              comfort of their homes.{" "}
+            </p>
+
+            <br />
+
+            <p>
+              Since our inception, we've worked tirelessly to curate a diverse
+              selection of high-quality products that cater to every taste and
+              preference. From fashion and beauty to electronics and home
+              essentials, we offer an extensive collection sourced from trusted
+              brands and suppliers
+            </p>
+           <br />
+            <p>
+             <b> Our Mission </b>
+            </p>
+            <br />
+            <p>Our mission at Forever is to empower customers with choice, convenience, and 
+              confidence. We're dedicated to providing a seamless shopping experience that exceeds
+               expectations, from browsing and ordering to delivery and beyond.</p>
+          </div>
+        </div>
       </div>
     </div>
   );
