31c038d0065f45677b7a0eae94f67d3bfcfb0a0d
sanjeevini <sanjeevinimnaik@gmail.com>
2026-01-28 14:50:31 +0530

    all commit

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import '../styles/Contact.css'
 
 const Contact = () => {
   return (
     <div>
         <Navbar />
-        <h1>Contact Us</h1>
+        <div className="contact">
+          <div className="contact-us">CONTACT US</div>
+          <div className="contact-details">
+            <div className="contact-pic">
+          </div>
+            <div className="contact-info">
+              
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
