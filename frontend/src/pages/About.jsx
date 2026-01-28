e9e250ae93783f484ea8583e046af8f79d05b073
PalakshShetty1 <shettypalakash5@email.com>
2026-01-28 12:52:51 +0530

    initial about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar";
+import "../styles/About.css";
 
 const About = () => {
   return (
     <div>
       <Navbar />
       <h1>About Us</h1>
+      <p></p>
+      <div className="main">
+        ddwd
+        <div className="left"></div>
+        <div className="right"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
