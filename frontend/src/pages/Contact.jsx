3fc6f707589133efbc8473a42ec177361b9ea671
manunaik128 <manumn2005@gmail.com>
2026-01-31 12:35:20 +0530

    latest

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -8,43 +8,47 @@ import Footer from '../components/Footer'
 const Contact = () => {
   return (
     <div>
-        <Navbar />
-        <div className="contact">
-          <div className="contact-us"> CONTACT US</div>
-          <div className="contact-details">
-            <div className="contact-pic">
-              <img src={contact} alt="" />
+      <Navbar />
+      <div className="contact">
+        <div className="contact-us"> CONTACT US</div>
+        <div className="contact-details">
+          <div className="contact-pic">
+            <img src={contact} alt="" />
           </div>
-            <div className="contact-info">
-              <div className="our-store">
-                <h2><b>Our Store</b></h2>
-              </div>
-              <div className="address"><h3>
-                54709 Willms Station <br />
-                Suite 350, Washington,
-                <br /> USA</h3>
-              </div>
-              <div className="tel"> <h3>
-                Tel: (415) 555-0132 <br />
-                Email: <br /> 
-                admin@forever.com </h3>
-              </div>
-              <div className="careers"><h3><b>Careers at Forever</b></h3></div>
-              <div className="learn"><p><h4>
-                Learn more about our <br />
-                 teams and job <br />
-                  openings.</h4></p></div>
-              <div className="explore"> 
-                <button >Explore Job's</button>
-              </div>
-     
+          <div className="contact-info">
+            <div className="our-store">
+              <h2><b>Our Store</b></h2>
             </div>
+            <div className="address"><h3>
+              54709 Willms Station <br />
+              Suite 350, Washington,
+              <br /> USA</h3>
+            </div>
+            <div className="tel"> <h3>
+              Tel: (415) 555-0132 <br />
+              Email: <br />
+              admin@forever.com </h3>
+            </div>
+            <div className="learn">
+              <h4>
+                Learn more about our <br />
+                teams and job <br />
+                openings.
+              </h4>
+            </div>
+
+            <div className="explore">
+              <button>Explore Jobs</button>
+            </div>
+
+
           </div>
-        
+        </div>
+
         <Discount />
 
-        <Footer />  
-    </div>
+        <Footer />
+      </div>
     </div>
   )
 }
