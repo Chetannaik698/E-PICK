ae0c15ff2914a891150d8392dc22e4ee5e5d1e1c
Chetan Naik <chetannaik11145@gmail.com>
2026-02-02 10:18:02 +0530

    routes created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,18 @@
-import { React } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home'
-import About from './Pages/About'
-import Collection from './pages/Collection'
-import Contact from './Pages/Contact'
-import Cart from './Pages/Cart'
-import ProductDetails from './pages/ProductDetails'
-import Admin from './AdminPage/Admin'
-import Dashboard from './AdminPage/Dashboard'
-import Orders from './pages/Orders'
-import Checkout from './pages/Checkout'
+import { React } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Collection from "./pages/Collection";
+import Contact from "./Pages/Contact";
+import Cart from "./Pages/Cart";
+import ProductDetails from "./pages/ProductDetails";
+import Admin from "./AdminPage/Admin";
+import Dashboard from "./AdminPage/Dashboard";
+import Orders from "./pages/Orders";
+import Checkout from "./pages/Checkout";
 
 function App() {
   return (
-
-
     <>
       <Router>
         <Routes>
@@ -31,7 +29,7 @@ function App() {
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
