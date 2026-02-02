f790f6e62afd9b40fa6ef1bfd61caf7e39d090ea
manunaik128 <manumn2005@gmail.com>
2026-02-02 10:02:59 +0530

    text added

--- frontend/src/AdminPage/Dashboard.jsx
+++ frontend/src/AdminPage/Dashboard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import '../AdminPage/Admin.css'
 
 const Dashboard = () => {
   return (
     <div>
       dashboard
+      <h1 className='mn'>hiii</h1>
     </div>
   )
 }
