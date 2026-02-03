7a0a21e92aa4919b7e072921097d28ffc7569f53
Chetan Naik <chetannaik11145@gmail.com>
2026-02-03 11:56:04 +0530

    frontend api integration

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,6 +9,12 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("You are on the server");
