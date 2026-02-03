7ab18a55dfd69bb82093a5a44964d0f0fae0a56b
Chetan Naik <chetannaik11145@gmail.com>
2026-02-03 07:22:56 +0530

    implemented axios and context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { React } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home";
+import Home from "./pages/Home";
 import About from "./Pages/About";
 import Collection from "./pages/Collection";
 import Contact from "./Pages/Contact";
