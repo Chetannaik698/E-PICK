77cd85c52cd5ae0d56ccaf2c3ec9995c42ce6fc6
Chetan Naik <chetannaik11145@gmail.com>
2026-04-03 12:23:51 +0530

    new change

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://e-pick-2k3k.vercel.app/",
+    origin: ["https://e-pick-2k3k.vercel.app", "http://localhost:5173"],
     credentials: true,
   }),
 );
